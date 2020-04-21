baa43dc996afb79a941643ae5837ff8766c11f4b
Floris <floriscapo@gmail.com>
2020-04-21 16:08:34 +0100

    Mise à jour

--- utilisateur/models.py
+++ utilisateur/models.py
@@ -11,6 +11,12 @@ CHOIX_TYPE_UTILISATEUR = (
 )
 
 
+# Choix du sexe
+CHOIX_SEXE = (
+    ('M', 'M'),
+    ('F', 'F')
+)
+
 #############################################################
 #                       UTILISATEUR                         #
 #############################################################
@@ -18,25 +24,25 @@ class Utilisateur(models.Model):
     Matricule = models.UUIDField(max_length=10, primary_key=True, unique=True, blank=False)
     Nom = models.CharField(max_length=20, blank=False)
     Prenom = models.CharField(max_length=50, blank=False)
-    Sexe = models.CharField(max_length=1, choices=('M', 'F'), blank=False)
+    Sexe = models.CharField(max_length=1, choices=CHOIX_SEXE, blank=False)
     DateDeNaissance = models.DateField()
-    Adresse = models.CharField(blank=False)
+    Adresse = models.CharField(blank=False, max_length=255)
     Mail = models.EmailField(blank=False)
-    Pays = models.CharField(blank=False)
-    Ville = models.CharField(blank=False)
-    Mobile = models.CharField(blank=False)
-    Superviseur = models.CharField(blank=True)
+    Pays = models.CharField(blank=False, max_length=30)
+    Ville = models.CharField(blank=False, max_length=30)
+    Mobile = models.CharField(blank=False, max_length=20)
+    Superviseur = models.CharField(blank=True, max_length=100)
     Pseudo = models.CharField(max_length=10, blank=False, unique=True)
     MotDePasse = models.CharField(max_length=10, blank=False, unique=True)
-    Type_Utilisateur = models.CharField(choices=CHOIX_TYPE_UTILISATEUR, blank=False)
+    Type_Utilisateur = models.CharField(choices=CHOIX_TYPE_UTILISATEUR, blank=False, max_length=10)
 
     #############################################################
     #                         STAGIAIRE                         #
     #############################################################
     Filiere = models.CharField(max_length=20, blank=True)
-    CV_lien = models.CharField(blank=True)
-    LettreDeRecommandation_Lien = models.CharField(blank=True)
-    LettreDeMotivation_Lien = models.CharField(blank=True)
+    CV_lien = models.FilePathField(blank=True)
+    LettreDeRecommandation_Lien = models.FilePathField(blank=True)
+    LettreDeMotivation_Lien = models.FilePathField(blank=True)
 
     # Choix Status Matrimoniel
     CHOIX_SITUATION_MATRIMONIEL = (
@@ -50,16 +56,16 @@ class Utilisateur(models.Model):
     #                         EMPLOYE                           #
     #############################################################
     CIN = models.PositiveIntegerField(blank=True)
-    Status_matrimoniel = models.CharField(choices=CHOIX_SITUATION_MATRIMONIEL, blank=True)
+    Status_matrimoniel = models.CharField(choices=CHOIX_SITUATION_MATRIMONIEL, blank=True, max_length=10)
     Enfants = models.PositiveIntegerField(blank=True)
-    Telephone_Fixe = models.CharField()
-    Departement = models.CharField(choices=..., blank=True)
-    Fonction = models.CharField(choices=..., blank=True)
-    Type_de_Contrat = models.CharField(choices=..., blank=True)
+    Telephone_Fixe = models.CharField(max_length=20)
+    Departement = models.CharField(choices=..., blank=True, max_length=30)
+    Fonction = models.CharField(choices=..., blank=True, max_length=30)
+    Type_de_Contrat = models.CharField(choices=..., blank=True, max_length=20)
     Date_Entree = models.DateField(blank=True)
     Date_Sortie = models.DateField(blank=True)
-    Nom_Contact_dUrgence = models.CharField()
-    Telephone_Contact_dUrgence = models.CharField()
+    Nom_Contact_dUrgence = models.CharField(max_length=30)
+    Telephone_Contact_dUrgence = models.CharField(max_length=50)
 
 
 #############################################################
@@ -67,8 +73,8 @@ class Utilisateur(models.Model):
 #############################################################
 class Photo(models.Model):
     utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
-    Photo_Id = models.CharField(primary_key=True, blank=False)
-    Photo_Lien = models.CharField()
+    Photo_Id = models.CharField(primary_key=True, blank=False, max_length=10)
+    Photo_Lien = models.FilePathField()
 
 
 # Choix status permission
@@ -83,22 +89,22 @@ STATUS_PERMISSION = (
 #                                  PERMISSION                                    #
 ##################################################################################
 class Permission(models.Model):
-    Code_Permission = models.CharField(primary_key=True)
+    Code_Permission = models.CharField(primary_key=True, max_length=10)
     Permissionnaire = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
     Date_Permission = models.DateTimeField(default=timezone.now)
     Date_Debut = models.DateField()
     Date_Fin = models.DateField()
-    Motif = models.CharField()
-    Status = models.CharField(choices=STATUS_PERMISSION)
+    Motif = models.CharField(max_length=50)
+    Status = models.CharField(choices=STATUS_PERMISSION, max_length=10)
 
 
 ###################################################################################
 #                                 NOTES INTERNES                                  #
 ###################################################################################
 class Notes_Internes(models.Model):
-    Code_Note = models.CharField(blank=False, primary_key=True)
-    Destinateur = models.ForeignKey(Utilisateur, on_delete=models.DO_NOTHING)
-    Destinataire = models.ManyToManyField(Utilisateur)
+    Code_Note = models.CharField(blank=False, primary_key=True, max_length=30)
+    Destinateur = models.ForeignKey(Utilisateur, on_delete=models.DO_NOTHING, related_name='Destinateur')
+    Destinataire = models.ManyToManyField(Utilisateur, related_name='Destinataire')
     Titre = models.CharField(blank=True, max_length=200)
     Contenu = models.TextField(max_length=2500)
 
@@ -118,8 +124,8 @@ CHOIX_TYPE_CONGES = (
 #                            CONGES                            #
 ################################################################
 class Conges(models.Model):
-    Code_Conges = models.CharField(primary_key=True, blank=False)
-    Type_Conges = models.CharField(choices=CHOIX_TYPE_CONGES, blank=False)
+    Code_Conges = models.CharField(primary_key=True, blank=False, max_length=5)
+    Type_Conges = models.CharField(choices=CHOIX_TYPE_CONGES, blank=False, max_length=50)
 
 
 ##########################################
@@ -145,4 +151,4 @@ class Prendre_Conge(models.Model):
     conges = models.ForeignKey(Conges, on_delete=models.CASCADE)
     date = models.ForeignKey(Calendrier_Conge, on_delete=models.CASCADE)
     Duree = models.PositiveIntegerField()
-    Status = models.CharField(choices=STATUS_CONGES)
+    Status = models.CharField(choices=STATUS_CONGES, max_length=10)
