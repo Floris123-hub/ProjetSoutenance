efb652bd6f7b5acfd107acf1646e6c135b4c2913
ferjeux <floriscapo@gmail.com>
2020-07-08 17:40:44 +0100

    ...

--- GP/prod_settings.py
+++ GP/prod_settings.py
@@ -1,9 +1,9 @@
 from .settings import *
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'gest-perso.herokuapp.com']
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 SECRET_KEY = '$!6%^^%elh^63qdl=v2mrwfmv)603v&5w!*hi3y-&7306#hi*)'
