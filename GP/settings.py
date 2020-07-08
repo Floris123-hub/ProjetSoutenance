7567290abab24978637e5e6be203ffa7dd73b17d
ferjeux <floriscapo@gmail.com>
2020-07-08 15:59:42 +0100

    Profile de l'utilisateur

--- GP/settings.py
+++ GP/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = '()3qk5!rarp%-v5&ao^pqowa)t41$()67#m#b&qf!di+l#8#=e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '0b3ddbf6da66.ngrok.io']
+ALLOWED_HOSTS = ['127.0.0.1', 'gest-perso.herokuapp.com']
 
 # Application definition
 
